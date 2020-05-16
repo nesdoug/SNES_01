840d100b1a94e0fc449f84d542875486d33b8298
nesdoug <dougfraker@gmail.com>
2020-05-16 12:53:45 -0400

    edit
    edit

--- main.asm
+++ main.asm
@@ -1,3 +1,5 @@
+; example 1 SNES code
+
 .p816
 .smart
 
@@ -10,35 +12,35 @@
 
 .segment "CODE"
 
-;enters here in forced blank
+; enters here in forced blank
 main:
-.a16 ;just a standardized setting from init code
+.a16 ; just a standardized setting from init code
 .i16
 	phk
 	plb
 	
 	A8
-	stz pal_addr ;$2121 set color address to 0
-	lda #$1f	 ;palette low byte gggrrrrr
-				 ;1f = all the red bits
-	sta pal_data ;$2122 - write twice
-	lda #$00	 ;palette high byte -bbbbbgg
-	sta pal_data ;$2122
+	stz pal_addr ; $2121 set color address to 0
+	lda #$1f	 ; palette low byte gggrrrrr
+				 ; 1f = all the red bits
+	sta pal_data ; $2122 - write twice
+	lda #$00	 ; palette high byte -bbbbbgg
+	sta pal_data ; $2122
 	
-;-bbbbbgg gggrrrrr
-;black = $0000
-;red = $001f
-;green = $03e0
-;blue = $7c00
-;white = $7fff
+; -bbbbbgg gggrrrrr
+; black = $0000
+; red = $001f
+; green = $03e0
+; blue = $7c00
+; white = $7fff
 
-;turn the screen on (end forced blank)
-	lda #$0f
-	sta fb_bright ;$2100
+; turn the screen on (end forced blank)
+	lda #FULL_BRIGHT ; $0f
+	sta fb_bright ; $2100
 
-;note, nothing is active on the main screen,
-;so only the main background color will show.
-;$212c is the main screen register	
+; note, nothing is active on the main screen,
+; so only the main background color will show.
+; $212c is the main screen register	
 
 InfiniteLoop:	
 	jmp InfiniteLoop
